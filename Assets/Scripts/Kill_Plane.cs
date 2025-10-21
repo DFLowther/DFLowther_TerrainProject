f826f9bf2aff4248e22c93ff20f7c211a9ac080b
DFLowther <Duncan.froggy@gmail.com>
2025-10-21 15:30:05 -0500

    10/21 Update

---
+++ Assets/Scripts/Kill_Plane.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kill_Plane : MonoBehaviour
+{
+    public Transform player;
+    public float heightOffset = 0;
+    private bool hasCalledReload = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+            return;
+
+        if (player.position.y < heightOffset + transform.position.y)
+        {
+            if(!hasCalledReload)
+                {
+                    hasCalledReload = true;
+
+                    
+                }
+        }
+    }
+}
