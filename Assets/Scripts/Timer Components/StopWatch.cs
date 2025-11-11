0411320f133e7e2e3eccd7e0dab4baf20ddcb850
DFLowther <Duncan.froggy@gmail.com>
2025-11-11 11:17:58 -0600

    11/11 Update 1

--- Assets/Scripts/Timer Components/StopWatch.cs
+++ Assets/Scripts/Timer Components/StopWatch.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using TMPro;
 
 public class StopWatch : MonoBehaviour
 {
     public float timerTime = 0.0f;
+    public TMP_Text timerText;
 
     // Update is called once per frame
     void Update()
     {
         timerTime += Time.deltaTime;
+
+        timerText.text = "Time: " + timerTime;
     }
 }
